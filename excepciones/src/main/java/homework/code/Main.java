4b5e5f2cc248ee6fa88dbeebea05a3da955e362a
Rafael Beltran <rafabeltrans17@gmail.com>
2025-03-20 12:07:29 -0600

    added main to excepction practica

--- excepciones/src/main/java/homework/code/Main.java
+++ excepciones/src/main/java/homework/code/Main.java
@@ -3,9 +3,16 @@ package homework.code;
 public class Main {
     public static void main(String[] args) {
         ClassOne classOne = new ClassOne();
+        ClassWithTwoMethods classWithTwoMethods = new ClassWithTwoMethods();
 
+        // test con excepcion
+        classOne.makeDivisionWithValidation(4, 2);
+
+        // test con excepcion nula
         classOne.makeDivisionWithValidationTwo(1, 1);
 
+        // test clase con dos metodos
+        classWithTwoMethods.substractionResult(2, 1);
     }
 
 }
